26c08f0a36b9233e989abda684cff063b60a6761
alan8r <alan8r@gmail.com>
2023-01-04 13:18:18 -0800

    Add files via upload
    added character movement and additional "bump" animation to make the total animation more cohesive

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const gameEngine = new GameEngine();
 const ASSET_MANAGER = new AssetManager();
 
 ASSET_MANAGER.queueDownload("./wario walk sprites.png");
+ASSET_MANAGER.queueDownload("./wario bump sprites.png");
 
 ASSET_MANAGER.downloadAll(() => {
 	const canvas = document.getElementById("gameWorld");
--- wario.js
+++ wario.js
@@ -1,17 +1,33 @@
 class Wario{
     constructor(game) {
         this.game = game;
-        let sheet = ASSET_MANAGER.getAsset("./wario walk sprites.png");
-        this.animator = new Animator(sheet, 0, 0, 29, 32, 4, 0.12, true);
+
+        let walkSheet = ASSET_MANAGER.getAsset("./wario walk sprites.png");
+        this.walkAnimator = new Animator(walkSheet, 0, 0, 29, 32, 4, 0.12, true);
+        let bumpSheet = ASSET_MANAGER.getAsset("./wario bump sprites.png");
+        this.bumpAnimator = new Animator(bumpSheet, 0, 0, 29, 32, 4, 0.09, true);
+
+        this.x = 0;
+        this.speed = 1;
+
+        this.state = 1;     // 1 = walking, 2 = bouncing backwards
     };
 
     update() {
+        this.x += this.speed;
 
+        if (this.x < 0) {   // left edge of screen trigger
+            this.speed = 1;
+            this.state = 1;
+        } else if (this.x > 1024 - (29*4)) {    // right edge of screen trigger
+            this.speed = -6;
+            this.state = 2;
+        }
     };
 
     draw(ctx) {
-        this.animator.drawFrame(this.game.clockTick, ctx, 0, 0, 4);
+        if (this.state == 1) this.walkAnimator.drawFrame(this.game.clockTick, ctx, this.x, 8, 4);
+        else if (this.state == 2) this.bumpAnimator.drawFrame(this.game.clockTick, ctx, this.x, 0, 4);
     };
-
 
 }
